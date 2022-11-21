47c29a23f73c1be79eeeac71aa7920f552d412e5
Jonathan Strandberg <jonstrandberg7@gmail.com>
2022-11-21 20:37:12 +0000

    basic python function spacing

--- precourse_recap.py
+++ precourse_recap.py
@@ -3,6 +3,6 @@ name = "Jonathan"
 
 def simple_function(): 
     """Prints greeting + name + how are you?"""
-    print(greeting, name + "how are you?")
+    print(greeting, name + " how are you?")
     
 simple_function()
