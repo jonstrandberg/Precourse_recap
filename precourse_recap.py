a24b6960c0c86cd427a331890e37bbe6187722b6
Jonathan Strandberg <jonstrandberg7@gmail.com>
2022-11-21 20:32:55 +0000

    commit with recap on python fucniton

---
+++ precourse_recap.py
@@ -0,0 +1,8 @@
+greeting = "Good morning"
+name = "Jonathan"
+
+def simple_function(): 
+    """Prints greeting + name + how are you?"""
+    print(greeting, name + "how are you?")
+    
+simple_function()
